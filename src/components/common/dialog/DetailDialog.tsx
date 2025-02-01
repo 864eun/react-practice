7304b0ddb6aac1225a2fef7a2d94ba34e7845349
eun <eun00864@gmail.com>
2025-02-01 23:53:10 +0900

    다이알 코드 수정

--- src/components/common/dialog/DetailDialog.tsx
+++ src/components/common/dialog/DetailDialog.tsx
@@ -29,40 +29,22 @@ function DetailDialog({ data, handleDialog }: Props) {
         // 1. 로컬스토리지에 bookmark이라는 데이터가 없을 경우
         if (!getLocalStorage || getLocalStorage === null) {
             localStorage.setItem('bookmark', JSON.stringify([selected]))
-            toast('해당 이미지를 북마크에 저장하였습니다. 😄')
+            toast('해당 이미지를 북마크에 저장하였습니다.')
         } else {
             // 2. 해당 이미지가 이미 로컬스토리지 bookmark라는 데이터에 저장되어 있을 경우
             if (getLocalStorage.findIndex((item: CardDTO) => item.id === selected.id) > -1) {
-                toast('해당 이미지는 이미 북마크에 추가된 상태입니다. ❌')
+                toast('해당 이미지는 이미 북마크에 추가된 상태입니다.')
             } else {
                 // 3. 해당 이미지가 로컬스토리지 bookmark라는 데이터에 저장되어 있지 않을 경우 + bookmark라는 데이터에 이미 어떤 값이 담겨 있는 경우
                 const res = [...getLocalStorage]
                 res.push(selected)
                 localStorage.setItem('bookmark', JSON.stringify(res))
 
-                toast('해당 이미지를 북마크에 저장하였습니다. 😄')
+                toast('해당 이미지를 북마크에 저장하였습니다.')
             }
         }
     }
 
-    useEffect(() => {
-        const getLocalStorage = JSON.parse(localStorage.getItem('bookmark'))
-
-        if (getLocalStorage && getLocalStorage.findIndex((item: CardDTO) => item.id === data.id) > -1) {
-            setBookmark(true)
-        } else if (!getLocalStorage) return
-
-        // ESC Key 입력시, 다이얼로그 닫기
-        const escKeyDownCloseDialog = (event: any) => {
-            console.log('함수호출')
-            if (event.key === 'Escape') {
-                closeDialog(event)
-            }
-        }
-        // ESC Key를 눌렀을 때, 다이얼로그창 닫기
-        window.addEventListener('keydown', escKeyDownCloseDialog) // 위에 만들어 놓은 escKeyDownCloseDialog를 keydown했을 때, 이벤트로 등록한다.
-        return () => window.removeEventListener('keydown', escKeyDownCloseDialog)
-    }, [])
 
     return (
         <div className={styles.container} >
